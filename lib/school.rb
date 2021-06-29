be50ef2c925fe44a6afdd782e9317a77ce2a9341
github username <github email address>
2021-06-29 14:46:20 -0400

    commit tree caad4f8e256cbe5be6cffb64750885c3b6e6ce26

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+    attr_reader :name, :roster
+    
+    def initialize(school_name)
+        @name = school_name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        if @roster.key?(grade)
+            @roster[grade] << student_name
+        else
+            @roster[grade] = []
+            @roster[grade] << student_name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |k, v|
+            v.sort!
+        end
+    end
+
+end
